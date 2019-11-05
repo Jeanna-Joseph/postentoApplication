0aba7ac8828d74c9b4dcbfe6a176f1535db554c2
JeannaJ <jeanna.chandy99@gmail.com>
2019-11-05 08:26:12 -0500

    commit

---
+++ appTest/urls.py
@@ -0,0 +1,9 @@
+# appTest/urls.py
+from django.conf.urls import url
+from appTest import views
+
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()), 
+]
---
+++ postento/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PostentoConfig(AppConfig):
+    name = 'postento'
